b72dca9edffac5badb9ccb45d7810562f9019ff0
abdelev <abdelmalek.lf@gmail.com>
2020-10-07 16:43:33 +0200

    fixing settings

--- resto/settings.py
+++ resto/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'generatedmail2809@gmail.com'
-EMAIL_HOST_PASSWORD = 'abdelmalek__09'
+EMAIL_HOST_USER = '****'
+EMAIL_HOST_PASSWORD = '****'
